9a9282c862883cccc0ad932c2488d96de07758e9
djsreenu <djsreenu@gmail.com>
2026-04-14 07:37:32 +0530

    expense

---
+++ bakend.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+LOG_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOG_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e " $R Please run this script with root priveleges $N" tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N" tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"  tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started execution at : $(date)" | tee -a $LOG_FILE
+
+CHECK_ROOT
+
+dnf module disable nodejs -y &>>LOG_FILE
+VALIDATE $? "disable nodejs"
+
+dnf module enable nodejs:20 -y &>>LOG_FILE
+VALIDATE $> "Enable nodejs"
+
+dnf instal nodejs -y &>>LOG_FILE
+VALIDATE $? "Install nodejs"
+
+id expense &>>LOG_FILE
+if [$? -ne 0]
+then
+    echo "Expense user is not exist $G creating .. $N"
+    useradd expense tee -a $LOG_FILE
+    VALIDATE $? "Creating Expense user"
+else
+    echo -e "Expnese user already exists.. $Y SKIPPING...$N"
