2391118459541e18af1ec77cd0df046d6914a7ec
djsreenu <djsreenu@gmail.com>
2026-04-15 13:10:45 +0530

    expense

--- bakend.sh
+++ bakend.sh
@@ -42,7 +42,7 @@ VALIDATE $? "Enable nodejs"
 dnf install nodejs -y &>>LOG_FILE
 VALIDATE $? "Install nodejs"
 
-id expense &>>LOG_FILE
+id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo -e "Expense user is not exist $G creating .. $N"
@@ -51,7 +51,7 @@ then
 else
     echo -e "Expnese user already exists.. $Y SKIPPING...$N"
 
-Mkdir -p /app &>>LOG_FILE
+Mkdir -p /app &>>$LOG_FILE
 VALIDATE $? "Creating app folder"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOG_FILE
@@ -59,15 +59,15 @@ VALIDATE $? "Dowloading backend application code"
 
 cd /app
 rm -rf /app/*
-unzip /tmp/backend.zip &>>LOG_FILE
+unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting application code"
 
-npm install &>>LOG_FILE
+npm install &>>$LOG_FILE
 VALIDATE $? "Install dependency"
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
 
 #load the data before running backend
-dnf install mysql -y &>>LOG_FILE
+dnf install mysql -y &>>$LOG_FILE
 VALIDATE $? "Installing MySQL client"
 
 mysql -h 172.31.25.78 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG_FILE
